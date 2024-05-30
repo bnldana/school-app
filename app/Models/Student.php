10ccdc3315fb437aeaeddd5eb5abe8a51dc8ddab
bnldana <157359790+bnldana@users.noreply.github.com>
2024-05-30 12:07:02 +0200

    Add explanations

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,12 +9,15 @@ class Student extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['first_name', 'last_name', 'date_of_birth', 'address', 'group_id'];
+    // Déclaration des colonnes de la table
+    protected $fillable = ['first_name', 'last_name', 'date_of_birth', 'address', 'group_id']; 
 
-    public $timestamps = false; // Désactive les timestamps automatiques
+    public $timestamps = false; // Désactive les colonnes timestamps automatiques (created_at, updated_at)
 
+    // Définition de la relation entre Student et Group
     public function group()
     {
-        return $this->belongsTo(Group::class, 'group_id');
+        // Un étudiant appartient à un seul groupe
+        return $this->belongsTo(Group::class, 'group_id'); 
     }
 }
